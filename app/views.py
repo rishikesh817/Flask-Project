92b50168297f4d29d6aeee5c5a0c1f15b29f40a8
rishikesh817 <rishikeshbalaji2000@gmail.com>
2017-06-28 13:59:08 -0700

    Finished login stuff, gui finished, still doesn't actually do anything
    next step: databases

--- app/views.py
+++ app/views.py
@@ -30,5 +30,4 @@ def login():
         return redirect('/index')
     return render_template('login.html', 
                            title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+                           form=form)
