81914eca88cfb7596b9299eaf4f8178dd1b4c5ee
rishikesh817 <rishikeshbalaji2000@gmail.com>
2017-06-28 11:57:36 -0700

    Update

--- app/views.py
+++ app/views.py
@@ -1,6 +1,34 @@
 from app import app
+from flask import render_template
+from .forms import LoginForm 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Rishikesh'}  # fake user
+    posts = [  # fake array of posts
+    { 
+    	'author': {'nickname': 'John'},
+        'body': '24' 
+    },
+    {     
+
+  		'author': {'nickname': 'Susan'}, 
+        'body': '51' 
+        
+    }
+    
+    ]
+    return render_template("index.html", title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/rishikeshbalaji/Desktop/Flask-Project/flask"
+set -gx VIRTUAL_ENV "/Users/rishikeshbalaji/Desktop/FLASK/app/flask-skeleton/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
